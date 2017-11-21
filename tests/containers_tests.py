fc001305c4c2c952ff2e086b802076dee7223008
Roberto Tiella <tiella@fbk.eu>
2017-11-21 17:39:57 +0100

    - new file containing unit tests to assess tick_relative code fixing

--- tests/containers_tests.py
+++ tests/containers_tests.py
@@ -5,7 +5,7 @@ import midi.events
 
 class ContainerTestCase(TestCase):
 
-    def test_slice_tick_mode(self):
+    def test_pattern_slice_tick_mode(self):
 
 
         a = midi.containers.Pattern()
@@ -13,12 +13,28 @@ class ContainerTestCase(TestCase):
 
         tr.append(midi.events.NoteOnEvent(pitch=60, tick=100))
         tr.append(midi.events.NoteOffEvent(pitch=60, tick=100))
-
         a.append(tr)
-
         a.make_ticks_abs()
 
-        evs = a[0][:]
+        newa = a[:]
 
-        self.assertEqual(evs[0].tick,100)
-        self.assertEqual(evs[1].tick,200)
+        self.assertEqual(a.tick_relative,newa.tick_relative)
+
+        # evs = a[0][:]
+        #
+        # self.assertEqual(evs[0].tick,100)
+        # self.assertEqual(evs[1].tick,200)
+
+
+    def test_track_slice_tick_mode(self):
+
+        tr = midi.containers.Track()
+
+        tr.append(midi.events.NoteOnEvent(pitch=60, tick=100))
+        tr.append(midi.events.NoteOffEvent(pitch=60, tick=100))
+        tr.make_ticks_abs()
+
+        newtr = tr[:]
+
+        self.assertEqual(newtr.tick_relative,tr.tick_relative)
+
