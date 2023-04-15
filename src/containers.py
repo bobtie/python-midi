ad93d46393a003c8746a86bd55a9340904e93da9
Roberto Tiella <tiella@fbk.eu>
2023-04-15 20:20:52 +0200

    output of 2to3

--- src/containers.py
+++ src/containers.py
@@ -25,7 +25,7 @@ class Pattern(list):
         if isinstance(item, slice):
             indices = item.indices(len(self))
             return Pattern(resolution=self.resolution, format=self.format, tick_relative=self.tick_relative,
-                            tracks=(super(Pattern, self).__getitem__(i) for i in xrange(*indices)))
+                            tracks=(super(Pattern, self).__getitem__(i) for i in range(*indices)))
         else:
             return super(Pattern, self).__getitem__(item)
 
@@ -58,7 +58,7 @@ class Track(list):
     def __getitem__(self, item):
         if isinstance(item, slice):
             indices = item.indices(len(self))
-            return Track((super(Track, self).__getitem__(i) for i in xrange(*indices)), tick_relative=self.tick_relative)
+            return Track((super(Track, self).__getitem__(i) for i in range(*indices)), tick_relative=self.tick_relative)
         else:
             return super(Track, self).__getitem__(item)
 
